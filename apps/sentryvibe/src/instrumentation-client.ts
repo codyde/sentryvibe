786e986ecc1b7b38a8706c23114dbbb5226ecf96
Cody De Arkland <codydearkland@gmail.com>
2025-10-24 20:27:45 -0700

    Adding distributed tracing

--- apps/sentryvibe/src/instrumentation-client.ts
+++ apps/sentryvibe/src/instrumentation-client.ts
@@ -32,6 +32,7 @@ spotlight: true,
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  tracePropagationTargets: ['localhost:4000', /^https:\/\/localhost:\d+$/],
 });
 
 export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
